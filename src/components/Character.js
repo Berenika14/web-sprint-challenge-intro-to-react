bfe6b9909fc13e50f41554cfa6c9458ac06ecc30
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-12-10 17:36:02 -0500

    styled Character Component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,17 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 
-// Write your Character component here
 const StyledCharacters = styled.div`
   display: flex;
-  justify-content: center;
-  width: 70%;
+  justify-content: space-between;
+  margin-left: 15em;
+  padding: 35px;
+  border: 3px solid green;
+  width: 60%;
+  height: 100%;
+  color: white;
+  opacity: 0.6;
+  :hover {
+    opacity: 1;
+  }
+  background-color: black;
 `;
 
+//Write your Character component here
 const Character = (props) => {
-  const { charactersName } = props;
+  const { charactersName, year } = props;
 
-  return <StyledCharacters>{charactersName}</StyledCharacters>;
+  return (
+    <StyledCharacters>
+      {charactersName} {year}
+    </StyledCharacters>
+  );
 };
 
 export default Character;
